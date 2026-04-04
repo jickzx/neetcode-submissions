d1873c95adfb1ffa1658532265211c25ff0c7807
James <144916455+jickzx@users.noreply.github.com>
2026-04-04 11:45:03 +0100

    Add: factory - submission-1

---
+++ Design Patterns/factory/submission-1.py
@@ -0,0 +1,36 @@
+class Vehicle(ABC):
+    @abstractmethod
+    def getType(self) -> str:
+        pass
+
+class Car(Vehicle):
+    def getType(self) -> str:
+        return "Car"
+
+class Bike(Vehicle):
+    def getType(self) -> str:
+        return "Bike"
+
+class Truck(Vehicle):
+    def getType(self) -> str:
+        return "Truck"
+
+class VehicleFactory(ABC):
+    @abstractmethod
+    def createVehicle(self) -> Vehicle:
+        pass
+
+class CarFactory(VehicleFactory):
+    @abstractmethod
+    def createVehicle(self) -> Car:
+        pass
+
+class BikeFactory(VehicleFactory):
+    @abstractmethod
+    def createVehicle(self) -> Bike:
+        pass
+
+class TruckFactory(VehicleFactory):
+    @abstractmethod
+    def createVehicle(self) -> Truck:
+        pass
