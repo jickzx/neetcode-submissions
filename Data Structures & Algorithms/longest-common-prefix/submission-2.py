75aace03bd343f48a1f0a61a0996d3b3818283e6
James <144916455+jickzx@users.noreply.github.com>
2026-05-12 19:36:51 +0100

    Add: longest-common-prefix - submission-2

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        # read the first few indexes e.g, if index[0] = different index[0]:
+        # output the first letter, and etc.
+        print(strs)
+
+        # if there is no longest common prefix, return ""
+        # else:
+        #     return ""
