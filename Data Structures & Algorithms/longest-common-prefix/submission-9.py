d3c95d93d46071790b3f11cfd56750e14bbbda4b
James <144916455+jickzx@users.noreply.github.com>
2026-07-18 11:00:13 +0100

    Add: longest-common-prefix - submission-9

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-9.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = "" # empty string if no common prefix
+
+        for i in range(len(strs[0])): 
+            """iterate through all of the strings 
+                or it is not the shortest string"""
+            for s in strs: # iterate through every string and they have the same exact char at index i
+                if i == len(s) or s[i] != strs[0][i]: # if in bounds or if not equal, return the result
+                    return res
+            res += strs[0][i]
+        
