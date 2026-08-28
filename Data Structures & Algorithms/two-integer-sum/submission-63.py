44d9dfe132c41add3c0ffa3bf4d89ef8cb63b1a9
James <144916455+jickzx@users.noreply.github.com>
2026-08-28 01:59:17 +0100

    Add: two-integer-sum - submission-63

---
+++ Data Structures & Algorithms/two-integer-sum/submission-63.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # hash map
+        seen = {}
+
+        for i,j in range(nums):
+            # remember, when doing this, we want to go in 1 pass, making it o(n) time and memory as it depends on how many values there are
+            diff = target - j
+            if seen in diff:
+                return seen[diff], i
+            seen[j] = i
